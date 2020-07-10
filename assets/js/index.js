b7216b44f5ad10a2326769eccca4ac56db907bcf
Tosin Jimoh <jayoluwatosin@gmail.com>
2020-07-10 14:01:52 +0100

    Update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,24 @@
-const buttonShow = document.getElementById('btnShow')
-const buttonHide = document.getElementById('btnHide')
+const buttonShow = document.getElementById('btnShow');
+const buttonHide = document.getElementById('btnHide');
 
-const hide = document.getElementById('hidden')
+const hide = document.getElementById('hidden');
+const mobiHide = document.getElementById('mobi-hidden');
+
 
 buttonShow.addEventListener('click', () => {
-  hide.style.display = 'block'
-  buttonShow.style.display = 'none'
-  buttonHide.style.display = 'block'
-  buttonHide.style.textAlign = 'center'
-})
+  hide.style.display = 'block';
+  mobiHide.style.display = 'block';
+
+  buttonShow.style.display = 'none';
+  buttonHide.style.display = 'block';
+  buttonHide.style.textAlign = 'center';
+});
 
 buttonHide.addEventListener('click', () => {
-  hide.style.display = 'none'
-  console.log('mee')
-  buttonHide.style.display = 'none'
-  buttonShow.style.display = 'block'
-  buttonShow.style.textAlign = 'center'
-})
+  hide.style.display = 'none';
+  mobiHide.style.display = 'none';
+
+  buttonHide.style.display = 'none';
+  buttonShow.style.display = 'block';
+  buttonShow.style.textAlign = 'center';
+});
