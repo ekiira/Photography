2d398df2bb94a7baf858c6ecb56a9a87d1fcaf16
Tosin Jimoh <jayoluwatosin@gmail.com>
2020-07-14 03:16:34 +0100

    Updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,13 @@
 const buttonShow = document.getElementById('btnShow');
 const buttonHide = document.getElementById('btnHide');
 const hidewrapper = document.getElementById('hide-btn');
+const category = document.querySelectorAll(".categories");
 
 const hide = document.getElementById('hidden');
 const mobiHide = document.getElementById('mobi-hidden');
 
 const loader = document.querySelector(".loader");
+const elmnt = document.getElementById("gallery-wrapper");
 
 // View hidden portfolio images
 buttonShow.addEventListener('click', () => {
@@ -22,13 +24,28 @@ buttonHide.addEventListener('click', () => {
 
   buttonHide.style.display = 'none';
   buttonShow.style.display = 'block';
-  buttonShow.style.textAlign = 'center';
+
+  elmnt.scrollIntoView();
 });
 
-
 // Preloader fades after 3000ms  
-window.addEventListener("load", function () {
+window.addEventListener('load', function () {
   setTimeout(() => {
     loader.className += " hidden";
   }, 3000)
+});
+
+// Changing Portfolio Menu active state
+const links = [...category]
+
+links.map((link, i) => {
+  link.addEventListener('click', (event) => {
+    const inactiveLinks = links.filter((linkData, i) => linkData.innerHTML !== event.target.innerHTML);
+   
+    inactiveLinks.map(inaactive => {
+      inaactive.classList.remove("selected")
+    })
+
+    link.classList.add("selected")
+  }) 
 });
